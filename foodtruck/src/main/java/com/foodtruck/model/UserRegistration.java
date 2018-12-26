6049d0c81ec99c61ae0cc2a13d4bb6aab36dbfb3
AS <AS@192.168.0.229>
2018-12-26 07:47:17 +0100

    Commit throug Eclipse IDE

---
+++ foodtruck/src/main/java/com/foodtruck/model/UserRegistration.java
@@ -0,0 +1,127 @@
+package com.foodtruck.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+//import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity(name = "user_tab")
+@IdClass(UserRegistrationId.class)
+@Table(name = "user_tab")
+public class UserRegistration implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "loginType")
+	String loginType;
+	
+	@Id
+	@Column(name = "loginId")
+	String loginId;
+	
+	@Column(name = "password")
+	@JsonIgnore
+	private String password;
+	
+	@Column(name = "firstName")
+	private String firstName;
+	
+	@Column(name = "lastName")
+	private String lastName;
+	
+	@Column(name = "emailId")
+	private String emailId;
+	
+	@Column(name = "mobileNumber")
+	private String mobileNumber;
+
+	public UserRegistration() {
+
+	}
+
+	public UserRegistration(String loginType, String loginId, String password, String firstName, String lastName,
+			String emailId, String mobileNumber) {
+		super();
+		this.loginType = loginType;
+		this.loginId = loginId;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getLoginType() {
+		return loginType;
+	}
+
+	public void setLoginType(String loginType) {
+		this.loginType = loginType;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRegistration [loginType=" + loginType + ", loginId=" + loginId + ", password=" + password
+				+ ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId + ", mobileNumber="
+				+ mobileNumber + "]";
+	}
+
+}
