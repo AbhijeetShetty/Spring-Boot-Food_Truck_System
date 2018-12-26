b39e9e0da57d3b6907fb5284ceb320c8eeb1e39e
AbhijeetShetty <45390983+AbhijeetShetty@users.noreply.github.com>
2018-12-26 08:29:26 +0100

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodtruck.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.foodtruck.model.UserRegistration;
-
-public interface UserRepository extends CrudRepository<UserRegistration, String>{
-
-}
