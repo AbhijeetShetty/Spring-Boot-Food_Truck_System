53026bdd06ce937497b4d63e592353bd1ac3c81e
AbhijeetShetty <AbhijeetShetty001@gmail.com>
2019-03-02 21:00:29 +0100

    Added Components(Contact,Register,Login,NotFound)

--- foodtruck-UI/UserInterface/src/app/app.module.ts
+++ foodtruck-UI/UserInterface/src/app/app.module.ts
@@ -3,13 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import {Router, Routes, RouterModule} from "@angular/router";
+const appRoutes : Routes = [
+  {
+    path : 'register',
+    component: RegisterComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'',
+    component: RegisterComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component: NotFoundComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     AppRoutingModule
   ],
   providers: [],
