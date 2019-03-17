2dedc8f82d596206a2e5d258db87badfb1953830
AbhijeetShetty <AbhijeetShetty001@gmail.com>
2019-03-17 22:12:01 +0100

    Form Changes

--- foodtruck/src/main/java/com/foodtruck/controller/UserRegistrationController.java
+++ foodtruck/src/main/java/com/foodtruck/controller/UserRegistrationController.java
@@ -7,6 +7,7 @@ import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import com.foodtruck.service.FeedbackDAOService;
 import com.foodtruck.service.TruckDAOService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/foodtruck")
 public class UserRegistrationController {
 
