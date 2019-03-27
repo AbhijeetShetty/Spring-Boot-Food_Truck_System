6ce00fc7407d8ca0570958d3a5a5847120d72bbb
AbhijeetShetty <abhijeetshetty001@gmail.com>
2019-03-27 12:19:46 +0100

    added auth service and changes in login

---
+++ foodtruck-UI/UserInterface/src/app/services/authentication.service.spec.ts
@@ -0,0 +1,7 @@
+import { Authentication.Service } from './authentication.service';
+
+describe('Authentication.Service', () => {
+  it('should create an instance', () => {
+    expect(new Authentication.Service()).toBeTruthy();
+  });
+});
